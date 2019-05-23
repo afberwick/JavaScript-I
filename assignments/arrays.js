309fdfe4a8eba938198f353f9987c21185b93baa
Lambda_School_Loaner_102 <lambda_school_loaner_102@Lambda-School-Loaner-102s-MacBook-Pro.local>
2019-05-23 19:32:52 -0400

    arrays.js Challenge 2 done

--- assignments/arrays.js
+++ assignments/arrays.js
@@ -68,19 +68,21 @@ let inventory = [{"id":1,"car_make":"Lincoln","car_model":"Navigator","car_year"
 
 var getcarIDinfo = ['2011', 'Jeep', 'Wrangler'];
     
-for (let i = 0; i < getcarIDinfo.length; i++) {
+//for (let i = 0; i < getcarIDinfo.length; i++) {
 
-        getcarIDinfo[i]; 
+        //getcarIDinfo[i]; 
 
-        }   
+        //}   
 
         console.log(getcarIDinfo);
 
 
 // ==== Challenge 2 ====
 // The dealer needs the information on the last car in their inventory.  What is the make and model of the last car in the inventory?  Log the make and model into the console.
-let lastCar = 0;
-console.log();
+
+let lastCar = ['Lincoln', 'Towncar'];
+
+console.log(lastCar);
 
 // ==== Challenge 3 ====
 // The marketing team wants the car models listed alphabetically on the website. Sort all the car model names into alphabetical order and log the results in the console
